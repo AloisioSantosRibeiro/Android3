8d1015834632e22c7bca303c5af6d4151f7c5d45
Aloisio Ribeiro <aloisio@mobstats.com>
2014-03-16 21:24:36 +0000

    Adding Action Buttons - Specifying the Actions in XML

--- gen/android/v3/R.java
+++ gen/android/v3/R.java
@@ -21,10 +21,17 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int ic_action_dark_help=0x7f020000;
+        public static final int ic_action_light_help=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f080001;
+        public static final int action_ThemeColor=0x7f080005;
+        public static final int action_ThemeDark=0x7f080002;
+        public static final int action_ThemeDarkLight=0x7f080004;
+        public static final int action_ThemeImage=0x7f080006;
+        public static final int action_ThemeLight=0x7f080003;
+        public static final int action_help=0x7f080001;
         public static final int edit_message=0x7f080000;
     }
     public static final class layout {
@@ -32,16 +39,21 @@ public final class R {
         public static final int activity_main=0x7f030001;
     }
     public static final class menu {
-        public static final int display_message=0x7f070000;
-        public static final int main=0x7f070001;
+        public static final int menu_display_message=0x7f070000;
+        public static final int menu_main=0x7f070001;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
+        public static final int action_ThemeColor=0x7f050005;
+        public static final int action_ThemeDark=0x7f050002;
+        public static final int action_ThemeDarkLight=0x7f050004;
+        public static final int action_ThemeImage=0x7f050006;
+        public static final int action_ThemeLight=0x7f050003;
+        public static final int action_help=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int button_send=0x7f050004;
-        public static final int edit_message=0x7f050003;
-        public static final int hello_android=0x7f050002;
-        public static final int title_activity_display_message=0x7f050005;
+        public static final int button_send=0x7f050009;
+        public static final int edit_message=0x7f050008;
+        public static final int hello_android=0x7f050007;
+        public static final int title_activity_display_message=0x7f05000a;
     }
     public static final class style {
         /** 
