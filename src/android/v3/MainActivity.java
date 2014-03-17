91726b029c2914305b9ad64d9089bc0c1ece6228
Aloisio Ribeiro <aloisio@mobstats.com>
2014-03-17 11:19:27 +0000

    Responding to Action Menu

--- src/android/v3/MainActivity.java
+++ src/android/v3/MainActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 
@@ -22,8 +24,69 @@ public class MainActivity extends Activity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+
+    	// Eclipse code
+//    	getMenuInflater().inflate(R.menu.menu_main, menu);
+//      return true;
+    	
+    	// Android's page code
+    	MenuInflater inflater = getMenuInflater();
+    	inflater.inflate(R.menu.menu_main, menu);
+    	return super.onCreateOptionsMenu(menu);
+    }
+
+    
+    public boolean onOptionsItemSelected(MenuItem item) {
+    	// Handle presses on the action bar items
+    	switch (item.getItemId()) {
+    	case R.id.action_ThemeDark:
+    		fOpenDarkThemeActivity();
+    		return true;
+    	case R.id.action_ThemeLight:
+    		fOpenLightThemeActivity();
+    		return true;
+    	case R.id.action_ThemeDarkLight:
+    		fOpenDarkLightThemeActivity();
+    		return true;
+    	case R.id.action_ThemeColor:
+    		fOpenColorThemeActivity();
+    		return true;
+    	case R.id.action_ThemeImage:
+    		fOpenImageThemeActivity();
+    		return true;
+   		default:
+   			return super.onOptionsItemSelected(item);
+    	}
+    }
+    
+    
+    public void fOpenDarkThemeActivity() {
+    	Intent intent = new Intent(this, ThemeDarkActivity.class);
+    	startActivity(intent);
+    }
+    
+    
+    public void fOpenLightThemeActivity() {
+    	Intent intent = new Intent(this, ThemeLightActivity.class);
+    	startActivity(intent);
+    }
+    
+    
+    public void fOpenDarkLightThemeActivity() {
+    	Intent intent = new Intent(this, ThemeDarkLightActivity.class);
+    	startActivity(intent);
+    }
+    
+    
+    public void fOpenColorThemeActivity() {
+    	Intent intent = new Intent(this, ThemeColorActivity.class);
+    	startActivity(intent);
+    }
+    
+    
+    public void fOpenImageThemeActivity() {
+    	Intent intent = new Intent(this, ThemeImageActivity.class);
+    	startActivity(intent);
     }
     
     
