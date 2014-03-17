dfca66d2cdeedc5d27947929021e03d448ebe5f6
Aloisio Ribeiro <aloisio@mobstats.com>
2014-03-17 16:55:31 +0000

    Customized background (image) for Action Bar

--- gen/android/v3/R.java
+++ gen/android/v3/R.java
@@ -21,9 +21,10 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_action_dark_help=0x7f020000;
-        public static final int ic_action_light_help=0x7f020001;
-        public static final int ic_launcher=0x7f020002;
+        public static final int actionbar_background=0x7f020000;
+        public static final int ic_action_dark_help=0x7f020001;
+        public static final int ic_action_light_help=0x7f020002;
+        public static final int ic_launcher=0x7f020003;
     }
     public static final class id {
         public static final int action_ThemeColor=0x7f080005;
@@ -74,6 +75,9 @@ public final class R {
         public static final int title_activity_theme_light=0x7f05000e;
     }
     public static final class style {
+        /**  ActionBar styles 
+         */
+        public static final int ActionBarImage=0x7f060003;
         /** 
         Base application theme, dependent on API level. This theme is replaced
         by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
@@ -100,5 +104,8 @@ public final class R {
  All customizations that are NOT specific to a particular API-level can go here. 
          */
         public static final int AppTheme=0x7f060001;
+        /**  the theme applied to the application or activity 
+         */
+        public static final int CustomActionBarTheme=0x7f060002;
     }
 }
